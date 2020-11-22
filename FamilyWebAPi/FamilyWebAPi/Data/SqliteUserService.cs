714e8a36e7c2ca376b74f42658ffe3ca43ca8c37
pavliuc75 <56117353+pavliuc75@users.noreply.github.com>
2020-11-22 17:16:09 +0100

    1716

---
+++ FamilyWebAPi/FamilyWebAPi/Data/SqliteUserService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DNPAssigment1.Models;
+using FamilyWebAPi.DataAccess;
+
+namespace DNPAssigment1.Data
+{
+    public class SqliteUserService : IUserService
+    {
+        private FamilyDBContext ctx;
+
+        public SqliteUserService(FamilyDBContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public async Task<User> ValidateUserAsync(string userName, string password)
+        {
+            User first = ctx.Users.FirstOrDefault(user => user.UserName.Equals(userName
+            ));
+            if (first == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            if (!first.Password.Equals(password))
+            {
+                throw new Exception("Incorrect password");
+            }
+
+            return first;
+        }
+    }
+}
